33cfb05c143b91e3f967acf22db6d8b3b46d9d19
Teddy Faulk <FaulkDev@gmail.com>
2023-11-09 14:47:14 -0500

    finish two exercises from wb5

---
+++ exercises/ES6Scripts/es6_course_search.js
@@ -0,0 +1,58 @@
+let courses = [
+  {
+    CourseId: "PROG100",
+    Title: "Introduction to HTML/CSS/Git",
+    Location: "Classroom 7",
+    StartDate: "09/08/22",
+    Fee: "100.00",
+  },
+  {
+    CourseId: "PROG200",
+    Title: "Introduction to JavaScript",
+    Location: "Classroom 9",
+    StartDate: "11/22/22",
+    Fee: "350.00",
+  },
+  {
+    CourseId: "PROG300",
+    Title: "Introduction to Java",
+    Location: "Classroom 1",
+    StartDate: "01/09/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROG400",
+    Title: "Introduction to SQL and Databases",
+    Location: "Classroom 7",
+    StartDate: "03/16/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROJ500",
+    Title: "Introduction to Angular",
+    Location: "Classroom 1",
+    StartDate: "04/25/23",
+    Fee: "50.00",
+  },
+];
+// find prog200 start date if it exists
+
+// let desiredCourse = "PROG200";
+
+// const course = courses.find(course => course.CourseId == desiredCourse)
+
+// if (course){
+//     console.log(`Ladies and Gentleman we got em. ${desiredCourse} starts ${course.StartDate}`);
+// }else {console.log(`It got away`);}
+
+//find proj500 title if it exists
+
+// let desiredCourse = "PROJ500";
+
+// const course = courses.find((course) => course.CourseId == desiredCourse);
+
+// if (course) {
+//   console.log(`The title of ${desiredCourse} is ${course.Title}`);
+// } else {
+//   console.log(`Why you looking for something that doesn't exist`);
+// }
