2310f3bb95ad96614d9f93d45a64af8bf47e60bb
Teddy Faulk <FaulkDev@gmail.com>
2023-11-21 14:44:31 -0500

    create exercise files

---
+++ exercises/ArrayMethods/array-sort-objects.js
@@ -0,0 +1,5 @@
+let products = [
+    { prodId: 2, name: "Notepads (12 pk)", price: 12.29 },
+    { prodId: 12, name: "Black Pens (12 pk)", price: 5.7 },
+    { prodId: 22, name: "Stapler", price: 12.79 },
+  ];
