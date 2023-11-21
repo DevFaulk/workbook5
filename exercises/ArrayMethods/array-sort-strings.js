3acf5b3d5b8bd1f0231a59ddb5a56231c66e953e
Teddy Faulk <FaulkDev@gmail.com>
2023-11-21 14:46:33 -0500

    finish string array exercise

--- exercises/ArrayMethods/array-sort-strings.js
+++ exercises/ArrayMethods/array-sort-strings.js
@@ -1,1 +1,11 @@
-let cars = ["Saab", "Volvo", "BMW"];
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+cars.sort((a, b) => {
+  if (a < b) {
+    return -1;
+  } else if (a == b) {
+    return 0;
+  } else return 1;
+});
+
+console.log(cars);
